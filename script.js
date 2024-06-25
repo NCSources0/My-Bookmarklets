f953468b84f6c3061abbe80b09398c1180e0dc04
NCSources <104072199+NCResources@users.noreply.github.com>
2024-06-25 09:18:17 -0400

    It fixed yet?

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const codeDivs = document.querySelectorAll("div.code");
   codeDivs.forEach((div) => {
     const ogText = div.textContent;
+    const ogHTML = div.innerHTML;
+    div.innerHTML = null;
     const button = document.createElement("a");
     button.innerHTML = `<svg xmlns='http://www.w3.org/2000/svg' style='position:relative;top:4px;' height='20px' viewBox='0 -960 960 960' fill='#fff'><path d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'/></svg> Copy Code`;
     button.addEventListener("click", function () {
       copy(ogText);
     });
-    div.insertBefore(button, div.firstChild);
+    div.appendChild(button);
+    div.innerHTML += ogHTML;
   });
 });
 
