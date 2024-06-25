12e5d2f281d60f6c970bb17ffacc1abeaf4c06ba
NCSources <104072199+NCResources@users.noreply.github.com>
2024-06-25 08:28:12 -0400

    NoW It sHoUlD Be fIxEd!.!.!.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const codeDiv = document.querySelectorAll("div#code");
   codeDiv.forEach((div, index) => {
     const ogHTML = div.innerHTML;
+		const ogText = div.textContent;
     div.innerHTML = `<a><svg xmlns='http://www.w3.org/2000/svg' style='position:relative;top:4px;' height='20px' viewBox='0 -960 960 960' width='24px' fill='#fff'><path d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'/></svg>Copy Code</a>`;
-    div.querySelector("a").addEventListener("click", copy(ogHTML));
+    div.querySelector("a").addEventListener("click", copy(ogText));
     div.innerHTML += ogHTML;
   });
 });
