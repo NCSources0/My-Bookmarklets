36242dd7a91da881f30fca62454e0acb695104bd
NCSources <104072199+NCResources@users.noreply.github.com>
2024-06-25 09:03:19 -0400

    You: You fixed it yet
    Me: Myself, no

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const codeDiv = document.querySelectorAll("div#code");
-  codeDiv.forEach((div, index) => {
-    const ogHTML = div.innerHTML;
+  const codeDivs = document.querySelectorAll("div.code");
+  codeDivs.forEach((div) => {
     const ogText = div.textContent;
-    div.innerHTML = `<a><svg xmlns='http://www.w3.org/2000/svg' style='position:relative;top:4px;' height='20px' viewBox='0 -960 960 960' width='24px' fill='#fff'><path d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'/></svg>Copy Code</a>`;
-    div.querySelector("a").addEventListener(
-      "click",
-      (function () {
-        copy(ogText);
-      })()
-    );
-    div.innerHTML += ogHTML;
+    const button = document.createElement("a");
+    button.innerHTML = `<svg xmlns='http://www.w3.org/2000/svg' style='position:relative;top:4px;' height='20px' viewBox='0 -960 960 960' fill='#fff'><path d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'/></svg> Copy Code`;
+    button.addEventListener("click", function () {
+      copy(ogText);
+    });
+    div.insertBefore(button, div.firstChild);
   });
 });
 
 function copy(text) {
-  navigator.clipboard.writeText(text);
-  alert("Code copied to clipboard");
+  navigator.clipboard.writeText(text).then(
+    () => {
+      alert("Code copied to clipboard");
+    },
+    (err) => {
+      console.error("Could not copy text: ", err);
+    }
+  );
 }
