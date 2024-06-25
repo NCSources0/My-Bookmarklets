d3941d44d1b8f75e4919ffe2635fac7401263333
NCSources <104072199+NCResources@users.noreply.github.com>
2024-06-25 15:11:13 -0400

    I might have fixed the copy button.

--- script.js
+++ script.js
@@ -7,11 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const button = document.createElement("a");
     button.innerHTML =
       "<svg xmlns='http://www.w3.org/2000/svg' height='20px' viewBox='0 -960 960 960' fill='#fff'><path d='M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z'/></svg>";
-    button.addEventListener("click", function () {
-      copy(ogText);
-    });
+    button.addEventListener("mousedown", copy(ogText));
     div.appendChild(button);
-    console.log(div.innerHTML);
     div.innerHTML += ogHTML;
   });
 });
